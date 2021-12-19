88a35b36d82968d7853edb64cb0cba9d765b60a6
KienTran <kien.112.1998@gmail.com>
2021-12-19 12:06:57 +0700

    update home screen

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -1,1 +1,84 @@
-<footer class="footer"></footer>
+<div class="bg-vnu-gray" style="font-size: 12px">
+  <div class="container px-0 py-3">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-md-6">
+        <ul style="list-style-type: none">
+          <li class="mb-1"><strong style="font-size: 13px">GIỚI THIỆU</strong></li>
+          <li class="mb-1"><a class="text-dark" href="#">Giới thiệu về ĐHKT</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tin Nghiên cứu khoa học</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Cơ cấu tổ chức</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Đội ngũ giảng viên</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Liên hệ</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-md-6">
+        <ul style="list-style-type: none">
+          <li class="mb-1"><strong style="font-size: 13px">TIN TỨC</strong></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tin đào tạo</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Chương trình đào tạo</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Hoạt động đoàn thể</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tin tổ chức</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tuyển dụng</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-md-6">
+        <ul style="list-style-type: none">
+          <li class="mb-1"><strong style="font-size: 13px">CHƯƠNG TRÌNH ĐÀO TẠO</strong></li>
+          <li class="mb-1"><a class="text-dark" href="#">Giới thiệu hoạt động đào tạo</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Chương trình ĐT Cử nhân</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Chương trình ĐT Thạc sĩ</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Chương trình ĐT Tiến sĩ</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-md-6">
+        <ul style="list-style-type: none">
+          <li class="mb-1"><strong style="font-size: 13px">NGHIÊN CỨU KHOA HỌC</strong></li>
+          <li class="mb-1"><a class="text-dark" href="#">Giới thiệu hoạt động NCKH</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Nhóm nghiên cứu mạnh</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Thống kê</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-md-6">
+        <ul style="list-style-type: none">
+          <li class="mb-1"><strong style="font-size: 13px">TUYỂN SINH</strong></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tuyển sinh Cử nhân</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tuyển sinh Thạc sĩ</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tuyển sinh Tiến sĩ</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-md-6">
+        <ul style="list-style-type: none">
+          <li class="mb-1"><strong style="font-size: 13px">THÔNG TIN HỮU ÍCH</strong></li>
+          <li class="mb-1"><a class="text-dark" href="#">Web ĐHQG Hà Nội</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Thư viện Đại học Quốc gia HN</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Tài nguyên số</a></li>
+          <li class="mb-1"><a class="text-dark" href="#">Học liệu số</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-md-6"></div>
+      <div class="col-lg-3 col-md-6 col-md-6">
+        icon
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bg-vnu-blue" style="font-size: 12px">
+  <div class="container px-0 py-3 text-right text-white">
+    <ul style="list-style-type: none">
+      <li class="mb-1">Bản quyền thuộc về Trường Đại học Kinh tế - Đại học Quốc gia Hà Nội</li>
+      <li class="mb-1">Giấy phép số 3994/GP-TTĐT, do Sở Thông tin và Truyền Thông, UBND Thành phố Hà Nội cấp ngày 14/12/2015</li>
+      <li class="mb-1">144 đường Xuân Thủy, quận Cầu Giấy, Hà Nội.    Tel: (84.24) 37547506     Fax: (84.24) 37546765</li>
+      <li class="mb-1">Email: news_ueb@vnu.edu.vn</li>
+      <li class="mb-1">Website: http://ueb.edu.vn   -   http://ueb.edu.vn</li>
+    </ul>
+  </div>
+</div>
+<style type="text/css">
+  .bg-vnu-gray {
+    background-color: #e6e6e8;
+  }
+
+  .bg-vnu-blue {
+    background-color: var(--blue-vnu);
+  }
+</style>
