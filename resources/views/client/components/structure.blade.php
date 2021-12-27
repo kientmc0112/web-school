933d24c7e184f0496c0fc4dee44362cb3ae15992
Tu ĐD <tudd@fabbi.io>
2021-12-27 22:30:23 +0700

    structrure

---
+++ resources/views/client/components/structure.blade.php
@@ -0,0 +1,107 @@
+<div class=ninja-team>
+  <div class=col-12>
+    <div class=co-cau-to-chuc>
+      <div class="absolute-item hoi-dong-khoa-hoc-va-dao-tao">
+        <div class="rectangle left-connect bg-white text-uppercase">
+          hội đồng khoa học và đào tạo
+        </div>
+      </div>
+      <div class="absolute-item ban-dieu-hanh-cac-chuong-trinh">
+        <div class="rectangle left-connect bg-white text-uppercase">
+          tổ chuyên môn liên ngành
+        </div>
+      </div>
+      <div class="absolute-item ban-dieu-hanh-cac-chuong-trinh clone">
+        <div class="rectangle left-connect bg-white text-uppercase">
+          tổ chuyên môn liên ngành
+        </div>
+      </div>
+      <div class="absolute-item to-chuyen-mon-lien-nganh">
+        <div class="rectangle left-connect bg-white text-uppercase">
+          ban điều hành các chương trình
+        </div>
+      </div>
+      <div class="absolute-item to-chuyen-mon-lien-nganh">
+        <div class="rectangle left-connect bg-white text-uppercase">
+          ban điều hành các chương trình
+        </div>
+      </div>
+      <div class="absolute-item to-lien-nganh-khoa-hoc-xa-hoi-va-nhan-van">
+        <div class="rectangle right-connect bg-white text-uppercase">
+          Tổ liên ngành khoa học xã hội và nhan văn
+        </div>
+      </div>
+      <div class="absolute-item to-lien-nganh-khoa-hoc-quan-li-kinh-te-va-luat">
+        <div class="rectangle right-connect bg-white text-uppercase">
+          Tổ liên ngành khoa học quản lí, kinh tế và luật
+        </div>
+      </div>
+      <div class="absolute-item to-lien-nganh-khoa-hoc-tu-nhien-cong-nghe-va-ky-thuat">
+        <div class="rectangle right-connect bg-white text-uppercase">
+          Tổ liên ngành khoa học tự nhien, công nghệ và kỹ thuật
+        </div>
+      </div>
+      <div class=level-1>
+        <div class="rectangle text-uppercase">
+          Ban chủ nhiệm khoa
+        </div>
+      </div>
+      <div class="level-1 last">
+        <div class="rectangle text-uppercase">
+          Giảng viên kiêm nhiệm quốc tế
+        </div>
+      </div>
+
+      <div class="level-1 last">
+        <div class="rectangle text-uppercase cac-phong-ban">
+          Các phòng ban
+        </div>
+      </div>
+      <div class=level-2>
+        <div class=row>
+          <div class="col-9 col-connect-header pl-0 pr-0 offset-2">
+            <div class=header-connect></div>
+          </div>
+        </div>
+        <div class="row row-eq-height">
+          <div class=col-3>
+            <div class=big-pb-item>
+              <div class=pb-item>
+                <div class="rectangle bottom-connect top-connect connect-2-level text-uppercase">
+                  phòng hành chính tổng hợp
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=col-3>
+            <div class=big-pb-item>
+              <div class=pb-item>
+                <div class="rectangle bottom-connect top-connect connect-2-level text-uppercase">
+                  phòng đào tạo và công tác sinh viên
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=col-3>
+            <div class=big-pb-item>
+              <div class=pb-item>
+                <div class="rectangle bottom-connect top-connect connect-2-level text-uppercase">
+                  phòng khoa học công nghệ hợp tác phát triển
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=col-3>
+            <div class=big-pb-item>
+              <div class=pb-item>
+                <div class="rectangle bottom-connect top-connect connect-2-level text-uppercase">
+                  bộ phận tuyển sinh và tuyển sinh
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>    
+</div>
