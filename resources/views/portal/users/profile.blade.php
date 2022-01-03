5b001d8afe0f7ec40857608de6b75385e8b4159f
KienTran <kien.112.1998@gmail.com>
2022-01-03 18:41:14 +0700

    add info attr user table

--- resources/views/portal/users/profile.blade.php
+++ resources/views/portal/users/profile.blade.php
@@ -23,8 +23,8 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-3"></div>
-    <div class="col-lg-6">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <img id="avatar" src="{{ isset($user->avatar) ? $user->avatar : "https://www.minervastrategies.com/wp-content/uploads/2016/03/default-avatar.jpg" }}" alt="Flowers in Chania"> 
@@ -75,6 +75,25 @@
                                     @endforeach
                                 </select>
                             </div>
+                            <div class="form-group">
+                                <label>{{ trans('messages.user.label.info') }}</label>
+                                <ul class="nav nav-tabs">
+                                    <li class="active">
+                                        <a href="#info" data-toggle="tab" aria-expanded="false">Vietnamese</a>
+                                    </li>
+                                    <li class="">
+                                        <a href="#info_en" data-toggle="tab" aria-expanded="false">English</a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content">
+                                    <div class="tab-pane fade in active" id="info">
+                                        <textarea name="info" placeholder="Content....." class="form-control" id="editor" rows="10">{!! $user->info !!}</textarea>
+                                    </div>
+                                    <div class="tab-pane fade" id="info_en">
+                                        <textarea name="info_en" placeholder="Content....." class="form-control" id="editor_en" rows="10">{!! $user->info_en !!}</textarea>
+                                    </div>
+                                </div>
+                            </div>
                             <div style="text-align: right">
                                 <button type="submit" class="btn btn-default">{{ trans('messages.user.button.submit') }}</button>
                                 <button type="reset" class="btn btn-default">{{ trans('messages.user.button.reset') }}</button>
@@ -85,11 +104,12 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-3"></div>
+    <div class="col-lg-2"></div>
 </div>
 @endsection
 
 @section('js')
+    <script src="//cdn.ckeditor.com/4.17.1/full/ckeditor.js"></script>
     <script>
         $(document).ready(function() {
             $("#avatar").click(function() {
@@ -99,6 +119,53 @@
                 var tmppath = URL.createObjectURL(e.target.files[0]);
                 $("#avatar").fadeIn("fast").attr('src',URL.createObjectURL(e.target.files[0]));
             })
+            CKEDITOR.replace('editor', {
+                filebrowserBrowseUrl: '{{ route('ckfinder_browser') }}',
+                toolbarGroups: [
+                    { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+                    { name: 'styles', groups: [ 'styles' ] },
+                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+                    { name: 'forms', groups: [ 'forms' ] },
+                    { name: 'colors', groups: [ 'colors' ] },
+                    { name: 'tools', groups: [ 'tools' ] },
+                    '/',
+                    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+                    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+                    { name: 'links', groups: [ 'links' ] },
+                    { name: 'insert', groups: [ 'insert' ] },
+                    '/',
+                    { name: 'others', groups: [ 'others' ] },
+                    { name: 'about', groups: [ 'about' ] }
+                ],
+                removeButtons: 'Source,Save,Templates,NewPage,ExportPdf,Preview,Print,Find,Replace,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CopyFormatting,CreateDiv,PageBreak,Iframe,ShowBlocks,About,Smiley,Anchor',
+                editorplaceholder: 'Type your content...',
+                height: '300'
+            });
+
+            CKEDITOR.replace('editor_en', {
+                filebrowserBrowseUrl: '{{ route('ckfinder_browser') }}',
+                toolbarGroups: [
+                    { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+                    { name: 'styles', groups: [ 'styles' ] },
+                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+                    { name: 'forms', groups: [ 'forms' ] },
+                    { name: 'colors', groups: [ 'colors' ] },
+                    { name: 'tools', groups: [ 'tools' ] },
+                    '/',
+                    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+                    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+                    { name: 'links', groups: [ 'links' ] },
+                    { name: 'insert', groups: [ 'insert' ] },
+                    '/',
+                    { name: 'others', groups: [ 'others' ] },
+                    { name: 'about', groups: [ 'about' ] }
+                ],
+                removeButtons: 'Source,Save,Templates,NewPage,ExportPdf,Preview,Print,Find,Replace,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CopyFormatting,CreateDiv,PageBreak,Iframe,ShowBlocks,About,Smiley,Anchor',
+                editorplaceholder: 'Type your content...',
+                height: '300'
+            });
         })
     </script>
 @endsection
